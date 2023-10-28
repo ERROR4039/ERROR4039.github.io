d3da5dc9cec238394dd8b99afdaa09558c5bfef8
ERROR4039 <148512136+ERROR4039@users.noreply.github.com>
2023-10-28 15:12:33 -0500

    Add files via upload

---
+++ Python/Print.py
@@ -0,0 +1,9 @@
+def sum(a, b, c, d):
+    return (a + b + c + d)
+
+a = int(input('Enter 1 number: '))
+b = int(input('Enter 2 number: '))
+c = int(input('Enter 3 number: '))
+d = int(input('Enter 4 number: '))
+
+print(f'Sum of {a}, {b}, {c} and {d} is {sum(a, b, c, d)}')
